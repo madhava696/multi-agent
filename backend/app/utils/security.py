728431917ce7eacdfaf6017d40477a43d14381eb
Madhava Sai <madhavasaipeddi@gmail.com>
2026-07-18 23:13:23 +0530

    basic hashing password : 200

---
+++ backend/app/utils/security.py
@@ -0,0 +1,10 @@
+from passlib.context import CryptContext
+
+password_context = CryptContext(schemes=["pbkdf2_sha256"],depricated="auto")
+
+def hash_password(password:str)->str:
+    return password_context.hash(password)
+
+def verify_password(password:str,hashed_password:str)->str:
+    return password_context.verify(password,hashed_password)
+
